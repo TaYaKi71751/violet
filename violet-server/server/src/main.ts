68819be47919ef6ac2830faa7fb4ff340f7b6f28
violet-dev <violet.dev.master@gmail.com>
2025-05-04 15:52:59 +0900

    Enable cors

--- violet-server/server/src/main.ts
+++ violet-server/server/src/main.ts
@@ -12,6 +12,12 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookies());
 
+  // CORS 설정 추가
+  app.enableCors({
+    origin: true, // 모든 출처에서의 요청을 허용
+    credentials: true, // 쿠키와 인증 헤더를 포함한 요청 허용
+  });
+
   setupSwagger(app);
 
   await app.listen(3000);
