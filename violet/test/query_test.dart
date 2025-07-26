7ceb3f7d769e8b2de445d6800a54075ebb326214
violet-dev <violet.dev.master@gmail.com>
2025-07-26 20:09:25 +0900

    Fix tests

--- violet/test/query_test.dart
+++ violet/test/query_test.dart
@@ -43,7 +43,7 @@ void main() {
     });
 
     test('Hitomi Query To Sql', () {
-      Settings.searchPure.value = false;
+      Settings.searchPure.setValue(false);
       final result0 =
           translate2query('female:sole_female (lang:korean or lang:n/a)');
       final result1 =
