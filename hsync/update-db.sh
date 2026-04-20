c08abe309df7463c1293bb9a6022fc3b964d5926
TaYaKi71751 <shantian.tw@gmail.com>
2026-04-21 07:05:53 +0900

    Add update-db.sh

---
+++ hsync/update-db.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+UNAME=$(uname)
+UNAME_ARCHITECTURE=$(uname -m)
+OS="unknown"
+if [[ "$UNAME" == "Darwin" ]]; then
+    OS="osx"
+elif [[ "$UNAME" == "Linux" ]]; then
+    OS="linux"
+fi
+
+if [[ "$UNAME_ARCHITECTURE" == "x86_64" ]]; then
+    ARCH="amd64"
+elif [[ "$UNAME_ARCHITECTURE" == "arm64" ]]; then
+    ARCH="arm64"
+fi
+
+cd hsync
+dotnet publish -r ${OS}-${ARCH} -c Release /p:PublishSingleFile=true /p:PublishTrimmed=false /p:PublishReadyToRun=false
+cp ../sync.py bin/Release/net8.0/${OS}-${ARCH}/publish
+cd bin/Release/net8.0/${OS}-${ARCH}/publish
+./hsync
+python3 sync.py
